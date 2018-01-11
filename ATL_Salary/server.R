bc87a0ab3bc6a0eef00ef90c77760144d83f9410
Tmas-Jc <thomas.jcng@gmail.com>
2018-01-12 00:13:59 +0800

    Done first draft

--- ATL_Salary/server.R
+++ ATL_Salary/server.R
@@ -17,8 +17,11 @@ x_agegrp <- raw %>% group_by(age) %>% summarise(n = n()) %>% filter(n < 10) %>% 
 # Ethnic group that has a sample size smaller than 30
 x_ethgrp <- raw %>% group_by(ethnic) %>% summarise(n = n()) %>% filter(n < 30) %>% droplevels() %>% pull(ethnic) 
 
-# Filter small sample size group for both 'age' and 'ethnic'
-dat <- raw %>% filter(!age %in% x_agegrp &!ethnic %in% x_ethgrp)
+dat <- raw %>% 
+    # Filter small sample size group for both 'age' and 'ethnic'
+    filter(!age %in% x_agegrp &!ethnic %in% x_ethgrp) %>% 
+    # and simplify ethnic group to first word only
+    mutate(ethnic = factor(stringr::str_extract(ethnic, "^[A-Z]?[a-z]+")))
 
 ## Remove 61 rows of data
 # nrow(raw) - nrow(dat)
@@ -26,27 +29,87 @@ dat <- raw %>% filter(!age %in% x_agegrp &!ethnic %in% x_ethgrp)
 
 # Server functions -------------------------------------------------------------
 
-# Set ggplot2 theme
-old <- theme_set(theme_light() + theme(legend.position = "bottom"))
 
-# Explore various variables' effects on Salary
+# Explore 'sex', "age", or 'ethnic' variables on 'salary'
 shinyServer(function(input, output, session) {
     
-    # Main Panel - Scatterplot (Age + Sex / Ethnic)
-    # Enable user to select points using cursor
+    # Set ggplot2 theme
+    old <- theme_set(theme_light() + theme(legend.position = "none"))
+    
+    # Main window - scatterplot (Age + Sex / Ethnic)
     output$main <- renderPlot({
         dat %>% 
             ggplot(aes(age, salary, col = sex)) + 
             geom_point(position = position_jitter(width = 0.3, height = 0.1)) + 
             scale_y_continuous(labels = scales::dollar) + 
-            labs(x = "Age", y = "Salary", col = "Sex")
+            labs(x = "Age", y = "Median Salary", col = "Sex") + 
+            theme(legend.position = c(0.1, 0.9), legend.text = element_text(size = 12)) # retrieve legend just for this plot
     })
     
-    # Sideplot 1 - Lineplot (Age)
+    # All or User selected points
+    brushPts <- reactive({
+        if(is.null(input$main_brush)){
+            dat
+        }else{
+            brushedPoints(dat, input$main_brush)
+        }
+    })
+    
+    # Find median salary by age
+    grp_by_age <- reactive({
+        brushPts() %>% group_by(sex, age) %>% summarise(salary = median(salary))
+    })
+    
+    # Find median salary by ethnic
+    grp_by_ethnic <- reactive({
+        brushPts() %>% group_by(sex, ethnic) %>% summarise(salary = median(salary))
+    })
+    
+    # So that our plots can share a common y-axis
+    scaleY <- reactive({
+        # Get range of y-axis value
+        rng_y <- range(grp_by_age()$salary, grp_by_ethnic()$salary)
+        scale_y_continuous(labels = scales::dollar, limits = rng_y)
+    })
+        
+    # Plot 1 - lineplot (Sex + Age)
+    output$age <- renderPlotly({
+        p <- grp_by_age() %>%  
+            ggplot(aes(age, salary, col = sex, group = sex,
+                       # Custom tooltip
+                       text = paste("Sex:", sex, "\nAge:", age, "\nSalary:", salary))) + 
+            geom_line() + 
+            geom_rug(sides = "l") +
+            scaleY() + 
+            labs(x = "Age", y = "", col = "Sex")
+        
+        # Convert to interactive plot
+        ggplotly(p, tooltip = c("text")) 
+    })
+    
+    # Get ethnic group salary range for geom_linerange
+    linerange <- reactive({
+        brushPts() %>% group_by(sex, ethnic) %>% summarise(salary = median(salary)) %>% 
+            # Convert to long format so that we can do comparison
+            tidyr::spread(sex, salary) %>% 
+            mutate(ymax = ifelse(Female > Male, Female, Male), ymin = ifelse(Female < Male, Female, Male))
+    })
+    
+    # Plot 2 - dumbbell plot (Sex + Ethnic)
+    output$ethnic <- renderPlotly({
+        p <- grp_by_ethnic() %>% 
+            ggplot(aes(ethnic, salary, col = sex, 
+                       text = paste("Sex:", sex, "\nEthnic:", ethnic, "\nSalary:", salary))) + 
+            geom_linerange(aes(x = ethnic, ymin = ymin, ymax = ymax), data = linerange(), inherit.aes = FALSE, col = "darkgrey") + 
+            # It is important to put geom_point last so that it overlaps the lines
+            geom_point(size = 3) + 
+            scaleY() +
+            labs(x = "Ethnic", y = "", col = "Sex") + 
+            # Hide this y-axis, we have one on the left already
+            theme(axis.text.y = element_blank())
+        
+        ggplotly(p, tooltip = c("text"))
+    })
     
     
-    # Sideplot 2 - Dumbellplot (Sex + Ethnic)
-    
-    # Sideplot 3 - Pointplot (Department)
-  
 })
--- ATL_Salary/ui.R
+++ ATL_Salary/ui.R
@@ -1,21 +1,29 @@
 library(shiny)
+library(shinythemes)
 library(plotly)
 
 # Define UI for application 
 shinyUI(fluidPage(
-  
+    theme = shinytheme("paper"),
   # Application title
-  titlePanel("Atlanta City Employee Salaries 2015"),
-  
+  tags$h4("Atlanta City Employee Salaries 2015"),
   # Sidebar with instruction and input params
-  sidebarLayout(
-    sidebarPanel(
-        
+  fluidRow(
+    column(4,
+        wellPanel(includeMarkdown("about.md")),
+        verbatimTextOutput("brushInfo")
     ),
-    
-    
-    mainPanel(
-        shiny::plotOutput('main')
+    column(8,
+        # Enable user to select points using cursor
+        tags$p("SELECTOR \t (Click and drag to select points)"),
+        plotOutput('main', brush = brushOpts(id = "main_brush"), height = 400),
+        # Split horizontal space evenly
+        tags$p("PLOT I & II"),
+        splitLayout(
+            plotlyOutput("age"),
+            plotlyOutput("ethnic")
+        ),
+    hr() # Just a little more space
     )
   )
 ))
