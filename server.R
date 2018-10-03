4421f591b97758cdfc03249e2986a51345309fb8
Thomas Jc <thomas.jcng@gmail.com>
2018-10-03 19:32:35 +0800

    Code review after 8 months.

--- server.R
+++ server.R
@@ -1,25 +1,33 @@
 library(shiny)
-library(dplyr)
-library(ggplot2)
+library(tidyverse)
 library(plotly)
 
 
 # Data pre-processing -----------------------------------------------------
 
-raw <- readRDS("atl_2015.RDS") %>% 
-    rename(ethnic = ethnic.origin, job = job.title, salary = annual.salary) %>% 
-    mutate(gender = factor(sex), ethnic = factor(ethnic)) %>% 
+# Read data from file
+raw <- readRDS("Data/atl_2015.RDS") %>% 
+    rename(ethnic = ethnic.origin, 
+           job = job.title, 
+           salary = annual.salary) %>% 
+    mutate(gender = factor(sex), 
+           ethnic = factor(ethnic)) %>% 
     as_tibble()
 
-# Age group that has a sample size smaller than 10
-x_agegrp <- raw %>% group_by(age) %>% summarise(n = n()) %>% filter(n < 10) %>% pull(age) 
+# A count helper
+which_less_than <- function(vec, n) {
+    x <- table(vec) < n
+    sort(unique(vec))[x]
+} 
 
-# Ethnic group that has a sample size smaller than 30
-x_ethgrp <- raw %>% group_by(ethnic) %>% summarise(n = n()) %>% filter(n < 30) %>% droplevels() %>% pull(ethnic) 
+# Id age group that has a sample size smaller than 10
+sub_age <- with(raw, age %>% which_less_than(10))
+# Id ethnic group that has a sample size smaller than 30
+sub_ethnic <- with(raw, ethnic %>% which_less_than(30))
 
+# Filter small sample size group for both 'age' and 'ethnic'
 dat <- raw %>% 
-    # Filter small sample size group for both 'age' and 'ethnic'
-    filter(!age %in% x_agegrp &!ethnic %in% x_ethgrp) %>% 
+    filter(!(age %in% sub_age), !(ethnic %in% sub_ethnic)) %>% 
     # and simplify ethnic group to first word only
     mutate(ethnic = factor(stringr::str_extract(ethnic, "^[A-Z]?[a-z]+")))
 
@@ -36,10 +44,15 @@ shinyServer(function(input, output, session) {
     output$selector <- renderPlot({
         dat %>% 
             ggplot(aes(age, salary)) +
+            # enable toggle of input button
             aes_string(col = input$col) +
             geom_point(position = position_jitter(width = 0.3, height = 0.1)) + 
-            scale_y_continuous(labels = scales::dollar, limits = c(min(dat$salary), max(dat$salary) + 5e4)) + 
-            labs(x = "Age", y = "Salary (USD)", col = toupper(as.character(input$col))) + 
+            scale_y_continuous(labels = scales::dollar, 
+                               # a little buffer to make it looks nicer
+                               limits = c(min(dat$salary), max(dat$salary) + 5e4)) + 
+            labs(x = "Age", 
+                 y = "Salary (USD)", 
+                 col = toupper(as.character(input$col))) + 
             # retrieve legend for this plot only
             theme(
                 legend.position = c(0.1, 0.8), 
@@ -50,86 +63,129 @@ shinyServer(function(input, output, session) {
             )
     })
     
-    # All or User selected points
+    # User selected points
     brushPts <- reactive({
         # when none selected, return all rows
-        if(is.null(input$selector_brush)){
+        if(is.null(input$selector_brush)) {
             return(dat)
-        }else{
-            df <- brushedPoints(dat, input$selector_brush)
-            # error handling: Make sure user selects some mininum points
-            if(nrow(df) > 10){
-                df
-            }else{
-                dat
-            }
+        }
+        # using Shiny default brush selector
+        subs <- brushedPoints(dat, input$selector_brush)
+        # defensive: make sure user selects some mininum points
+        if(nrow(subs) > 10) {
+            subs
+        } else{
+            dat
         }
     })
     
     # Find median salary by age
+    # to pass to plot 1
     grp_by_age <- reactive({
-        brushPts() %>% group_by(gender, age) %>% summarise(salary = median(salary))
+        brushPts() %>% 
+            group_by(gender, age) %>% 
+            summarise(salary = median(salary)) %>% 
+            ungroup()
     })
     
     # Find median salary by ethnic
+    # to pass to plot 2
     grp_by_ethnic <- reactive({
-        brushPts() %>% group_by(gender, ethnic) %>% summarise(salary = median(salary))
+        brushPts() %>% 
+            group_by(gender, ethnic) %>% 
+            summarise(salary = median(salary)) %>% 
+            ungroup()
     })
     
     # So that our plots can share a common y-axis
-    scaleY <- reactive({
-        # Get range of y-axis value
-        rng_y <- range(grp_by_age()$salary, grp_by_ethnic()$salary)
-        scale_y_continuous(labels = scales::dollar, limits = rng_y)
+    axisy <- reactive({
+        # get range of y-axis value
+        range_y <- range(grp_by_age()$salary, grp_by_ethnic()$salary)
+        scale_y_continuous(labels = scales::dollar, limits = range_y)
     })
         
     # Plot 1 - lineplot (Gender + Age)
     output$age <- renderPlotly({
-        p <- grp_by_age() %>%  
-            ggplot(aes(age, salary, col = gender, group = gender,
-                       # Custom tooltip
-                       text = paste("Gender:", gender, "\nAge:", age, "\nSalary:", salary))) + 
-            geom_line() + 
+        p <- grp_by_age() %>%
+            ggplot(aes(
+                age,
+                salary,
+                col = gender,
+                group = gender,
+                # custom tooltip
+                text = paste("Gender:", gender,
+                             "\nAge:", age,
+                             "\nSalary:", salary)
+            )) +
+            geom_line() +
             geom_rug(sides = "l") +
-            scaleY() + 
-            labs(x = "", y = "", col = "Gender", title  = "Plot I: Salary By Age Group") 
+            axisy() +
+            labs(
+                x = "",
+                y = "",
+                col = "Gender",
+                title  = "Plot I: Salary by Age Group"
+            )
         
-        # Convert to interactive plot
-        ggplotly(p, tooltip = c("text")) 
+        # convert to interactive
+        ggplotly(p, tooltip = c("text"))
     })
     
     # Get ethnic group salary range for geom_linerange
     linerange <- reactive({
         
-        ## error handling for too few samples
+        ## defence against too few samples
         if(length(unique(grp_by_ethnic()$gender)) < 2){
             return(NULL)
         }
         
-        brushPts() %>% group_by(gender, ethnic) %>% summarise(salary = median(salary)) %>% 
-            # Convert to long format so that we can do comparison
+        # First we compute the median salary 
+        # and determine the range
+        brushPts() %>% 
+            group_by(gender, ethnic) %>% 
+            summarise(salary = median(salary)) %>% 
+            # convert to long format so that we can do comparison
             tidyr::spread(gender, salary, fill = 0) %>% 
-            mutate(ymax = ifelse(Female > Male, Female, Male), ymin = ifelse(Female < Male, Female, Male))
+            # pick maximum and maximum rowwise (female vs male)
+            rowwise() %>% 
+            mutate(ymax = max(Female, Male),
+                   ymin = min(Female, Male))
     })
     
     # Plot 2 - dumbbell plot (Gender + Ethnic)
     output$ethnic <- renderPlotly({
-        
-        ## error handling: If only single gender is selected
-        if(length(unique(grp_by_ethnic()$gender)) < 2){
-            # Return a base object, not plotting anything meaningful
+        ## defensive: If only single gender is selected
+        if (length(unique(grp_by_ethnic()$gender)) < 2) {
+            # return a base object, not plotting anything meaningful
             p <- ggplot(aes(ethnic, salary), data = brushPts())
             return(ggplotly(p))
         }
         
-        p <- grp_by_ethnic() %>% 
-            ggplot(aes(ethnic, salary, col = gender, 
-                       text = paste("Gender:", gender, "\nEthnic:", ethnic, "\nSalary:", salary))) + 
-            geom_linerange(aes(x = ethnic, ymin = ymin, ymax = ymax), data = linerange(), inherit.aes = FALSE, col = "darkgrey") + 
+        p <- grp_by_ethnic() %>%
+            ggplot(aes(
+                ethnic,
+                salary,
+                col = gender,
+                text = paste("Gender:", gender,
+                             "\nEthnic:", ethnic,
+                             "\nSalary:", salary)
+            )) +
+            geom_linerange(
+                aes(x = ethnic, ymin = ymin, ymax = ymax),
+                # a reactive func passed down from above
+                data = linerange(),
+                inherit.aes = FALSE,
+                col = "darkgrey"
+            ) +
             # It is important to put geom_point last so that it overlaps the lines
-            geom_point(size = 3) + 
-            scaleY() +
-            labs(x = "", y = "", col = "Gender", title = "Plot II: Salary By Ethnic Group") + 
+            geom_point(size = 3) +
+            axisy() +
+            labs(
+                x = "",
+                y = "",
+                col = "Gender",
+                title = "Plot II: Salary By Ethnic Group"
+            ) +
             # Hide this y-axis, we have one on the left already
             theme(axis.text.y = element_blank())
         
@@ -138,7 +194,7 @@ shinyServer(function(input, output, session) {
     
     # Summary table
     output$summ <- renderPrint({
-        # A simple contingency table
+        # a simple contingency table
         with(brushPts(), table("Gender" = gender, "Ethnic" = ethnic))
     })
     
